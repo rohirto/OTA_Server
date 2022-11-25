1f4e8b3bf8d43c63e4fcaff661bdb767b9b779f6
rohirto <rohirto@gmail.com>
2022-11-25 13:43:04 +0530

    load store alignment bug
    Causing controller to rebbot

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.get("/one_relay_update", function (req, res) {
    }
 });
 
-const FOUR_REALY_LAST_VERSION = '1.0.10'
+const FOUR_REALY_LAST_VERSION = '1.0.11'
 app.get("/four_relay_update", function (req, res) {
     console.log(req.headers);
 
